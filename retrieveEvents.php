bd4f724aa32eac64d6d541779f010dd9472062aa
James <choo1991@uw.edu>
2013-06-02 21:03:16 -0700

    Retrieve events should work. Read comments for the categories

--- retrieveEvents.php
+++ retrieveEvents.php
@@ -27,8 +27,30 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 			echo json_encode(array('success' => 0));
 		}
 	} else if ($requestType == "categories") {
-		$requestCategories = "SELECT e.id, e.title, e.datetime, e.location, e.blurb, e.ownerId, c.title FROM Attending a LEFT JOIN Events e ON e.id = a.eventId 
-			LEFT JOIN Users u ON u.id = e.ownerId RIGHT JOIN Category c ON c.eventId = e.id WHERE c.title = :category";
+
+		/* CHECK THE VALUE OF THE userId INDEX OF THE ARRAY. IF IT'S EQUAL TO THE VALUE OF THE ID FOUND IN THE COOKIE, THEN IT'S SOMETHING
+			THEY'VE EITHER SAVED OR IS ATTENDING (CHECK THE STATUS 1 = ATTENDING, 0 = SAVING) 
+
+			OTHERWISE, THEY DID NOT SAVE IT OR SAY THEY'RE ATTENDING*/
+
+		$category => $_GET['category'];
+
+		// $requestCategories = "SELECT e.id, e.title, e.datetime, e.location, e.blurb, e.ownerId, c.title FROM Attending a LEFT JOIN Events e ON e.id = a.eventId 
+		// 	LEFT JOIN Users u ON u.id = e.ownerId RIGHT JOIN Category c ON c.eventId = e.id WHERE c.title = :category";
+		$requestCategories = "SELECT e.id, e.title, e.datetime, e.location, e.blurb, e.ownerId, a.userId, a.status, c.title 
+			FROM Category c LEFT JOIN Events e ON e.id = c.eventId LEFT JOIN Users u ON u.id = e.ownerId LEFT JOIN Attending a 
+			ON a.eventId = e.id WHERE c.title = :category AND a.userId = :id UNION 
+			(SELECT e.id, e.title, e.datetime, e.location, e.blurb, e.ownerId, a.userId, a.status, c.title FROM Category c 
+			LEFT JOIN Events e ON e.id = c.eventId LEFT JOIN Users u ON u.id = e.ownerId LEFT JOIN Attending a ON a.eventId = e.id 
+			WHERE c.title ='Nature' GROUP BY e.id and a.userId)";
+
+		$request = $db->prepare($requestCategories);
+		if ($request->execute(array('category' => $category, 'id' => $id)) {
+			echo json_encode($request);
+		} else {
+			echo json_encode(array('success' => 0));
+		}
+		
 	}
 
 
