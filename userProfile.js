b9c7fbce30ac06882fd8c86b2828a62422c66cbb
Josh Villars <jvillars19@gmail.com>
2013-06-05 15:57:49 -0700

    fixed some of the userProfile pages so that it dynamically loads information from the db

--- userProfile.js
+++ userProfile.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	$("#navEvents").addClass("active").siblings().removeClass("active");
+	$("#notification").hide();
 	var id = $.cookie('username');
 	$.ajax("retrieveEvents.php",{
 		data: {
@@ -13,4 +14,26 @@ $(document).ready(function(){
 
 function displayResults(data){
 	console.log(data);
+	if(data.length>0){
+		$("#defaultContent").hide();
+		$("#home div ul.right button").each(function(index, button){
+			console.log(button);
+			button.disabled = false;
+		});
+		$.each(data, function(index, val){
+			console.log(val);
+			var overallDiv = $("<div>").addClass("result");
+			var headline = $("h3").addClass("eventHeadline").text(val.title);
+			var portrait = $("<img>").attr("src","img/placeholderEvent.png");
+			var startDate = $("<span>").addClass("startDate").text(val.startDate);
+			var endDate = $("<span>").addClass("endDate").text(val.endDate);
+			var description = $("<p>").text(val.blurb);
+			var location = $("p").text(val.location);
+			var label = $("<a>").addClass("label").attr("href","joinEvent.php").text("View");
+			overallDiv.append(headline, portrait, startDate,endDate,description,location,label);
+			$("#allContent").append(overallDiv);
+		});	
+	}
+	
 }
+
