0fac6a2c47e131d81e189d213f5c1aceeaddeb3a
Josh Villars <jvillars19@gmail.com>
2013-06-05 22:25:51 -0700

    merging with main branch

--- events.js
+++ events.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 
-	console.log($.cookie('username'));
+	var cookieId = $.cookie('username'));
 
 });
--- userProfile.js
+++ userProfile.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	$("#navEvents").addClass("active").siblings().removeClass("active");
+	var id = $.cookie('username');
+	$.ajax("retrieveEvents.php",{
+		data: {requestType: 'categories', cookieId: id, category: 'foo'},
+		success: displayResults,
+		error: ajaxError
+	});
 });
+
+function displayResults(data){
+	console.log(data);
+	alert('foo');
+}
